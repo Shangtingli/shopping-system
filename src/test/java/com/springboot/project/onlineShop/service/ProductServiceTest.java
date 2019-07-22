16765a50a7ecb5f0e3e6c6ee7c810044b35698e7
Jason Li <jason.li11@sap.com>
2019-07-22 15:50:46 +0800

    and also asynchronously send email to user using rabbit

--- src/test/java/com/springboot/project/onlineShop/service/ProductServiceTest.java
+++ src/test/java/com/springboot/project/onlineShop/service/ProductServiceTest.java
@@ -11,7 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
-
+//Hello Wworld
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
